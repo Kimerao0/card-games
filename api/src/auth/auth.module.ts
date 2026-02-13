cbd41e3a52473e012412fe8d044d6361e068c691
Alessandro Ceruti <alessando@fido.id>
2026-02-13 10:53:41 +0100

    Added docker compose

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: 'DEV_SECRET_CHANGE_ME',
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
